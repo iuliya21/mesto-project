ee35b3178f36c745ae74db381f5fb0ca1b7669bd
iuliya21 <iuliya21@mail.ru>
2023-01-02 00:53:10 +0300

    fix: правки перед вебпаком

--- components/card.js
+++ components/card.js
@@ -1,4 +1,12 @@
+import { initialCards } from "./dates.js";
+import { openModal } from "./modal.js";
+
 const list = document.querySelector(".elements");
+const cardTemplateContent = document.querySelector("#template-card").content; //контент template
+const cardItem = cardTemplateContent.querySelector(".elements-item");
+const modalImage = document.querySelector(".popup_type_image"); //третий попап с увеличенным изображением
+const fullImage = document.querySelector(".popup-image__photo"); //фото из третьего попапа
+const imageOpenFullDescription = document.querySelector(".popup-image__description"); //подпись фото из третьего попапа
 
 //Функция удаления карточки
 export const removeCard = (element) => {
@@ -8,3 +16,36 @@ export const removeCard = (element) => {
 export function renderCard(element) {
   list.prepend(element);
 }
+
+//функция создания карточки
+export const createItem = (item) => {
+  const element = cardItem.cloneNode(true);
+  const elementName = element.querySelector(".elements-item__title");
+  const elementPhoto = element.querySelector(".elements-item__photo"); //фотография места
+  const btnRemove = element.querySelector(".elements-item__button");
+
+  const openImage = function () {
+    fullImage.alt = item.name;
+    fullImage.src = item.link;
+    imageOpenFullDescription.textContent = item.name;
+    openModal(modalImage);
+  };
+
+  btnRemove.addEventListener("click", () => removeCard(element));
+  elementName.textContent = item.name;
+  elementPhoto.src = item.link;
+  elementPhoto.alt = item.name;
+
+  elementPhoto.addEventListener("click", openImage);
+
+  element
+    .querySelector(".elements-item__like")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("elements-item__like_active");
+    });
+  return element;
+};
+
+initialCards.forEach((item) => {
+  renderCard(createItem(item));
+});
--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
-import { initialCards } from "./dates.js";
+
 import { enableValidation } from "./validate.js";
-import { removeCard, renderCard } from "./card.js";
 import { openModal, closeModal } from "./modal.js";
+import {  renderCard, createItem } from "./card.js";
 
 const buttonOpenPopupProfile = document.querySelector(".profile__button-pencil"); //кнопка редактирования имени и деятельности
 const buttonOpenPopupCard = document.querySelector(".profile__button"); //кнопка добавления новой карточки
@@ -10,19 +10,13 @@ const modalCreateCard = document.querySelector(".popup_type_card");
 const placeInput = modalCreateCard.querySelector(".popup__form-text_input_place");
 const linkInput = modalCreateCard.querySelector(".popup__form-text_input_link");
 const formPlace = modalCreateCard.querySelector(".popup__form"); //форма для второго попапа
-const modalImage = document.querySelector(".popup_type_image"); //третий попап с увеличенным изображением
-const fullImage = document.querySelector(".popup-image__photo"); //фото из третьего попапа
 const editPhotoProfile = document.querySelector(".popup_type_profile-photo");
-const imageOpenFullDescription = document.querySelector(".popup-image__description"); //подпись фото из третьего попапа
 const nameText = document.querySelector(".profile__title");
 const jobText = document.querySelector(".profile__paragraph");
 const formElement = document.querySelector(".popup__form"); //форма для первого попапа
 const nameInput = formElement.querySelector(".popup__form-text_input_name"); //инпут Имя
 const jobInput = formElement.querySelector(".popup__form-text_input_job"); //инпут Род деятельности
 const buttonCreateCard = formPlace.querySelector(".popup__button"); //кнопка "создать"
-
-const cardTemplateContent = document.querySelector("#template-card").content; //контент template
-const cardItem = cardTemplateContent.querySelector(".elements-item");
 const profilePhoto = document.querySelector(".profile__photo");
 const profilePhotoEdit = document.querySelector(".profile__photo-edit");
 
@@ -36,39 +30,6 @@ profilePhotoEdit.addEventListener("mouseout", () => {
 
 profilePhotoEdit.addEventListener("mouseover", () => {
   profilePhotoEdit.style.visibility = "visible";
-});
-
-//функция создания карточки
-const createItem = (item) => {
-  const element = cardItem.cloneNode(true);
-  const elementName = element.querySelector(".elements-item__title");
-  const elementPhoto = element.querySelector(".elements-item__photo"); //фотография места
-  const btnRemove = element.querySelector(".elements-item__button");
-
-  const openImage = function () {
-    fullImage.alt = item.name;
-    fullImage.src = item.link;
-    imageOpenFullDescription.textContent = item.name;
-    openModal(modalImage);
-  };
-
-  btnRemove.addEventListener("click", () => removeCard(element));
-  elementName.textContent = item.name;
-  elementPhoto.src = item.link;
-  elementPhoto.alt = item.name;
-
-  elementPhoto.addEventListener("click", openImage);
-
-  element
-    .querySelector(".elements-item__like")
-    .addEventListener("click", function (evt) {
-      evt.target.classList.toggle("elements-item__like_active");
-    });
-  return element;
-};
-
-initialCards.forEach((item) => {
-  renderCard(createItem(item));
 });
 
 //Функция сохранения информации в новую карточку
